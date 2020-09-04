586e505e3adce2e7baef648e3aacbd4262bc5109
Roy Spencer <roy@Roys-MacBook-Pro.local>
2020-09-04 17:38:52 -0400

    testing classes

--- Views/Wrapper.php
+++ Views/Wrapper.php
@@ -4,8 +4,8 @@ class Wrapper {
     public function header() {
         ?>
         <heading>
-            <div style="background-color:red">
-                Roy Spencer
+            <div style="background-color:#82002f; color:whitesmoke; padding:15px; 10px;">
+                <h1>Roy Spencer -- Testing</h1>
             </div>
         </heading>
         <?php
--- index.php
+++ index.php
@@ -16,25 +16,6 @@ $(document).ready( function () {
 } );
 </script>
 
-<table id="table_id" class="display">
-    <thead>
-        <tr>
-            <th>Column 1</th>
-            <th>Column 2</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-            <td>Row 1 Data 1</td>
-            <td>Row 1 Data 2</td>
-        </tr>
-        <tr>
-            <td>Row 2 Data 1</td>
-            <td>Row 2 Data 2</td>
-        </tr>
-    </tbody>
-</table>
-
 </div>
 <?php
 
@@ -58,3 +39,22 @@ function helloWorld() {
 }
 
 ?>
+
+<table id="table_id" class="display">
+    <thead>
+        <tr>
+            <th>Column 1</th>
+            <th>Column 2</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>Row 1 Data 1</td>
+            <td>Row 1 Data 2</td>
+        </tr>
+        <tr>
+            <td>Row 2 Data 1</td>
+            <td>Row 2 Data 2</td>
+        </tr>
+    </tbody>
+</table>
