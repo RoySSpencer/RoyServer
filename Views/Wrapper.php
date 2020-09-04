0f88909d05e9cdf3bcd5945e94d1d21f30addfb9
Roy Spencer <roy@Roys-MacBook-Pro.local>
2020-09-04 17:41:57 -0400

    testing classes

--- Views/Wrapper.php
+++ Views/Wrapper.php
@@ -1,10 +1,11 @@
+<link rel="stylesheet" href="../assests/css/wrapper.css">
 <?php
 
 class Wrapper {
     public function header() {
         ?>
         <heading>
-            <div style="background-color:#82002f; color:whitesmoke; padding:15px; 10px;">
+            <div id="header">
                 <h1>Roy Spencer -- Testing</h1>
             </div>
         </heading>
