789c84c517e789f585d5c895065cfb5ba9319b47
Roy Spencer <roy@Roys-MacBook-Pro.local>
2020-09-25 16:41:37 -0400

    start of project 1

--- Project1/ViewManager.php
+++ Project1/ViewManager.php
@@ -33,14 +33,21 @@ class ViewManager {
             if ($this->db->checkLogin()){
                 return true;
             } else {
+                $this->getLoggin();
                 echo "wrong username or password";
             }
-        }
+        } else {
+            $this->getLoggin();
+        } 
 
         echo "not logged in";
 
         return false;
 
+    }
+
+    public function getLoggin() {
+        echo "get loggin";
     }
 
 }
