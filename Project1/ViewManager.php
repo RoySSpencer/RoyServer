446d87f8f00633ad58906f6ec396e33be776a00f
Roy Spencer <roy@Roys-MacBook-Pro.local>
2020-09-25 11:51:10 -0400

    start of project 1

--- Project1/ViewManager.php
+++ Project1/ViewManager.php
@@ -4,12 +4,18 @@ namespace Project1;
 
 class ViewManager {
     public function router() {
+
+        linkHeader();
+        $wrapper = new Wrapper();
+        $wrapper->header("Roy Spencer -- Project List");
+        $wrapper->bodyStart();
+
         echo "hi";
+
+        $wrapper->bodyEnd();
+        $wrapper->footer();
+        
         
     }
 
-}
-
-function test() {
-    echo "hi";
 }
