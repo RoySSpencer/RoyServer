6583a2251f3aa0798d834ad1fe113d3ee40e45fe
Roy Spencer <roy@Roys-MacBook-Pro.local>
2020-09-25 17:35:12 -0400

    start of project 1

--- Authentication/Authentication.php
+++ Authentication/Authentication.php
@@ -26,6 +26,21 @@ class Authentication {
     }
 
     public function getLoggin() {
-        echo "get loggin";
+        $this->input();
+    }
+
+    public function input() {
+        ?>
+        <div class="row">
+            <div class="col-md-4">
+                Username:
+            </div>
+
+            <div class="col-md-8">
+                <input type="text" id="id" name="name">
+            </div>
+
+        </div>
+        <?php
     }
 }
