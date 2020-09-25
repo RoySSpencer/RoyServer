3cdc5e3ef0d7810f31ff8e2306c02a3375bc6d25
Roy Spencer <roy@Roys-MacBook-Pro.local>
2020-09-25 16:21:51 -0400

    start of project 1

--- Project1/Warehouse.php
+++ Project1/Warehouse.php
@@ -5,12 +5,12 @@ namespace Project1;
 class Warehouse{
 
     public function __construct() {
-        $servername = "localhost:3036";
+        // $servername = "localhost:3036";
         $username = "roy";
         $password = "toyroy";
 
         // Create connection
-        $conn = mysql_connect($servername, $username, $password);
+        $conn = mysql_connect($username, $password);
 
         // Check connection
         if (!$conn) {
