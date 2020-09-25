15a692199a6f3ba85c59dcfb5dcf43f9e9e9719d
Roy Spencer <roy@Roys-MacBook-Pro.local>
2020-09-25 11:33:27 -0400

    start of project 1

--- Project1/ViewManager.php
+++ Project1/ViewManager.php
@@ -2,12 +2,13 @@
 
 class ViewManager {
     public function router() {
+        echo "hi";
         $router = new \Bramus\Router\Router();
         $router->get('/P1/test', "test");
 
         $router->run();
 
-        echo "hi";
+        
     }
 
 }
