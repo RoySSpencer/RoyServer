c0f9a475ff9c38c3adbcce24c677385c767dfd97
RoySSpencer <spencersroy@gmail.com>
2020-09-25 21:20:40 -0400

    authentifaction

--- Authentication/LogginDatabase.php
+++ Authentication/LogginDatabase.php
@@ -11,26 +11,22 @@ class LogginDatabase{
         $db = "Passwords";
 
         // Create connection
-        $this->db = new mysqli($servername, $username, $password, $db);
+        $this->conn = mysqli_connect($servername, $username, $password, $db);
     }
 
     public function checkLogin() {
+        $sql = "select username, password from Logins";
+
+        $result = mysqli_query($conn, $sql);
+        // $rows = mysqli_fetch_row($result);
+
+        var_dump($_SESSION);
+
         if (!isset($_SESSION['loggedIn']['username']) || !isset($_SESSION['loggedIn']['password'])) {
             return false;
         }
 
-        $sql = "select username, password from Logins";
-
-        $result = $this->db->query($sql);
-        // $result = mysqli_query($this->conn, $sql);
-        // $rows = mysqli_fetch_row($result);
-
-        // var_dump($rows);
-        var_dump($_SESSION);
-
-
-
-        while($row = $result->fetch_assoc()) {
+        while($row = mysqli_fetch_assoc($result)) {
           var_dump($row);
             if ($row[0] == $_SESSION['loggedIn']['username'] && $row[1] == $_SESSION['loggedIn']['password']) {
                 return true;
