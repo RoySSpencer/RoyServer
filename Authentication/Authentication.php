45c12b08084ae84bf67ef2506521751a35da1038
RoySSpencer <spencersroy@gmail.com>
2020-09-26 17:27:10 -0400

    fetch item db

--- Authentication/Authentication.php
+++ Authentication/Authentication.php
@@ -58,9 +58,9 @@ class Authentication {
       $this->wrapper->nav(["/" => "Home", "/P1" => "Login"], $_SERVER['REQUEST_URI']);
       $this->wrapper->bodyStart();
 
-      // if (isset($_POST)) {
-      //   $this->postHandler();
-      // }
+      if (isset($_POST)) {
+        $this->postHandler();
+      }
 
       ?>
       <form method="post">
@@ -86,32 +86,32 @@ class Authentication {
         $_SESSION['loggedIn']['password'] = $_POST['password'];
         unset( $_POST['password']);
       }
-      // if (isset($_POST['newUsername']) || isset($_POST['newPassword']) || isset($_POST['newRepassword'])) {
-      //   // $this->newAccount();
-      // }
+      if (isset($_POST['newUsername']) || isset($_POST['newPassword']) || isset($_POST['newRepassword'])) {
+        $this->newAccount();
+      }
     }
 
-    // public function newAccount() {
-    //   if (isset($_POST['newUsername']) && isset($_POST['newPassword']) && isset($_POST['newRepassword'])) {
-    //     if ($_POST['newPassword'] == $_POST['newRepassword']) {
-    //       $this->db->createAcount($_POST['newUsername'], $_POST['newPassword']);
-    //       unset($_POST['newUsername']);
-    //       unset($_POST['newPassword']);
-    //       unset($_POST['newRepassword']);
-    //
-    //       header("location: /");
-    //
-    //       exit;
-    //     }
-    //   }
-    //   unset($_POST['newUsername']);
-    //   unset($_POST['newPassword']);
-    //   unset($_POST['newRepassword']);
-    //
-    //   header("location: /sign-up");
-    //
-    //   exit;
-    // }
+    public function newAccount() {
+      if (isset($_POST['newUsername']) && isset($_POST['newPassword']) && isset($_POST['newRepassword'])) {
+        if ($_POST['newPassword'] == $_POST['newRepassword']) {
+          $this->db->createAcount($_POST['newUsername'], $_POST['newPassword']);
+          unset($_POST['newUsername']);
+          unset($_POST['newPassword']);
+          unset($_POST['newRepassword']);
+
+          header("location: /");
+
+          exit;
+        }
+      }
+      unset($_POST['newUsername']);
+      unset($_POST['newPassword']);
+      unset($_POST['newRepassword']);
+
+      header("location: /sign-up");
+
+      exit;
+    }
 
     public function input($id, $name) {
         ?>
