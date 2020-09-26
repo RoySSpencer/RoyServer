beb778cb2b3ce5e9ab9c19d62d4f99a6475abdaf
RoySSpencer <spencersroy@gmail.com>
2020-09-25 22:42:09 -0400

    authentifaction

--- Project1/ViewManager.php
+++ Project1/ViewManager.php
@@ -43,6 +43,7 @@ class ViewManager {
       $loggedIn = false;
       if (isset($_SESSION['loggedIn'])) {
         $loggedIn = true;
+        echo "hi";
       }
       $this->wrapper->nav(["/" => "Home", "/P1" => "Data"], $_SERVER['REQUEST_URI'], $loggedIn);
       $this->wrapper->bodyStart();
